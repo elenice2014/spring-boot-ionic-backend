bff6f6d485ece0883201c94d91545bafec2fff77
“Elenice <elenice_2014@outlook.com>
2020-06-03 23:30:17 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/elenice/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.elenice.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.elenice.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
