e25cef7448a09f17a182da47db63e92c21f1f309
“Elenice <elenice_2014@outlook.com>
2020-06-29 22:30:49 -0300

    ItemPedido, ItemPedidoPK

--- src/main/java/com/elenice/cursomc/CursomcApplication.java
+++ src/main/java/com/elenice/cursomc/CursomcApplication.java
@@ -2,17 +2,16 @@ package com.elenice.cursomc;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.elenice.cursomc.domain.Categoria;
 import com.elenice.cursomc.domain.Cidade;
 import com.elenice.cursomc.domain.Cliente;
 import com.elenice.cursomc.domain.Endereco;
 import com.elenice.cursomc.domain.Estado;
+import com.elenice.cursomc.domain.ItemPedido;
 import com.elenice.cursomc.domain.Pagamento;
 import com.elenice.cursomc.domain.PagamentoComBoleto;
 import com.elenice.cursomc.domain.PagamentoComCartao;
@@ -25,6 +24,7 @@ import com.elenice.cursomc.repositories.CidadeRepository;
 import com.elenice.cursomc.repositories.ClienteRepository;
 import com.elenice.cursomc.repositories.EnderecoRepository;
 import com.elenice.cursomc.repositories.EstadoRepository;
+import com.elenice.cursomc.repositories.ItemPedidoRepository;
 import com.elenice.cursomc.repositories.PagamentoRepository;
 import com.elenice.cursomc.repositories.PedidoRepository;
 import com.elenice.cursomc.repositories.ProdutoRepository;
@@ -48,6 +48,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 
 	public static void main(String[] args) {
@@ -115,6 +117,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 	
 
--- src/main/java/com/elenice/cursomc/domain/Produto.java
+++ src/main/java/com/elenice/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.elenice.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,13 +13,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -30,6 +33,9 @@ public class Produto implements Serializable {
 
 	private List<Categoria> categorias = new ArrayList<>();
 
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Produto() {
 
 	}
@@ -39,6 +45,22 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	public Integer getId() {
