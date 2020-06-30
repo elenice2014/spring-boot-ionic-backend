898e25bb46afb67bc2b751fd057f5fc453165fbf
“Elenice <elenice_2014@outlook.com>
2020-06-29 23:08:04 -0300

    Atualização: utilizando somente jsonIgnore

--- src/main/java/com/elenice/cursomc/domain/Categoria.java
+++ src/main/java/com/elenice/cursomc/domain/Categoria.java
@@ -3,13 +3,12 @@ package com.elenice.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
@@ -20,7 +19,6 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference   //serializar o json para o response ficar organizado
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
