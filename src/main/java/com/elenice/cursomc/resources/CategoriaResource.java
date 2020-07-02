895fbafb36f8af2178aa97dd0b2f1aeb64dc582a
“Elenice <elenice_2014@outlook.com>
2020-07-01 21:09:23 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/elenice/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/elenice/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.elenice.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.elenice.cursomc.domain.Categoria;
 import com.elenice.cursomc.services.CategoriaService;
@@ -22,6 +26,16 @@ public class CategoriaResource {
 
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	  
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {   //@RequestBody faz o json ser convertido para o objeto Java automaticamente
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri(); //pega a url do Postam para inserir
+		return ResponseEntity.created(uri).build();
 
 	}
 }
