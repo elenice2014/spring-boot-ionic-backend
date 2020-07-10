c093c5a51d35a0552ef8a97891ba807be49464a1
“Elenice <elenice_2014@outlook.com>
2020-07-10 00:30:28 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/elenice/cursomc/services/ClienteService.java
+++ src/main/java/com/elenice/cursomc/services/ClienteService.java
@@ -51,7 +51,7 @@ public class ClienteService {
 			repo.deleteById(id);
 
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityViolationException("Não é possível excluir porque há entidades relacionadas!");
+			throw new DataIntegrityViolationException("Não é possível excluir porque há pedidos relacionadas!");
 		}
 	}
 
