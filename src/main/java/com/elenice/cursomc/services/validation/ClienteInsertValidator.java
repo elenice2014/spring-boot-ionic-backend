d311b6b9c7e8a4318452796536a607bbbf1b7d11
Elenice Carvalho Miranda Francisco <elenice.francisco@ibm.com>
2020-08-25 22:43:11 -0300

    Validacao customizada de CPF ou CNPJ na insercao de cliente

---
+++ src/main/java/com/elenice/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.elenice.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import com.elenice.cursomc.dto.ClienteNewDTO;
+import com.elenice.cursomc.resources.exception.FieldMessage;
+import com.elenice.cursomc.services.validation.utils.BR;
+import com.elenice.cursomc.domain.enums.*;
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+		    list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		 
+		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+		list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFielName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
