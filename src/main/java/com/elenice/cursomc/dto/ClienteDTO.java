4d6b5d4a011e0f57e3be15129ecfe71c0ea5b9ca
Elenice Carvalho Miranda Francisco <elenice.francisco@ibm.com>
2020-08-26 00:18:09 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/elenice/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/elenice/cursomc/dto/ClienteDTO.java
@@ -5,7 +5,10 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 import com.elenice.cursomc.domain.Cliente;
+import com.elenice.cursomc.services.validation.ClienteUpdate;
 
+
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
